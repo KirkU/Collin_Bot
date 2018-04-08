2af036b38d793e18bfbd9cef82f443dc78e2f1a6
KirkU <uihleinka@gmail.com>
2018-04-08 16:39:40 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,58 +7,7 @@ var isReady = true;
 //var forniteSucks = new Audio('480.mp4');
 
 client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', async message => {
-    
-    if(message.author.bot) return;
-    
-    //if(message.content.indexOf(config.prefix) !== 0) return;
-    
-    /*const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    
-    for (var i = 0; i < args.length; i++) {
-        
-        if(message.content.includes('aram') || message.content.includes('arams') || message.content.includes('league')) {
-            message.channel.send("League of Tanks, Game Never Changes!", {files: ["./assets/league_of_tanks.png"]});
-        }
-        
-        if(message.content.includes('fortnite')) {
-            message.channel.send({files: ["./assets/fortnite_sucks.jpg"]});
-            //Testing audio
-            message.channel.play({files: ["./assets/480.mp4"]});
-        }
-        
-    }*/
-    
-    if (message.content.includes('fortnite')) {
-        message.channel.send({files: ["./assets/fortnite_sucks.jpg"]});
-    }
-    
-    if (isReady && (message.content.includes('aram') || message.content.includes('arams') || message.content.includes('league'))) {
-        message.channel.send("League of Tanks, Game Never Changes!", {files: ["./assets/league_of_tanks.png"]});
-        
-        isReady = false;
-        var voiceChannel = message.member.voiceChannel;
-        
-        voiceChannel.join().then(connection => {
-            
-            const dispatcher = connection.playFile('./assets/league_file.mp3');
-            dispatcher.on("end", end => {
-                voiceChannel.leave();
-            });
-
-        }).catch(err => console.log(err));
-
-        isReady = true;
-        
-    }
-    
-    /*
-    if (message.author.username === 'HazyArc14') {
-        message.channel.send({files: ["./assets/fortnite_sucks.jpg"]});
-    }
+    console.log('I am
     */
 
 });
